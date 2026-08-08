e29307fa378e694a3e60fbfa9ee3641eab7afec7
Hansitha Ekanayaka <hansithaekanayaka1@gmail.com>
2026-08-09 02:16:23 +0530

    fix: remove open-next build artifacts from git tracking

--- .open-next/server-functions/default/.next/server/app/api/auth/callback/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/callback/route.js")
-R.c("server/chunks/[root-of-the-server]__1wy2raz._.js")
-R.c("server/chunks/node_modules_next_1_14bcs._.js")
-R.c("server/chunks/_0x678lm._.js")
-R.c("server/chunks/[root-of-the-server]__0l3yhx4._.js")
-R.c("server/chunks/_next-internal_server_app_api_auth_callback_route_actions_0v1dckr.js")
-R.m(42986)
-module.exports=R.m(42986).exports
